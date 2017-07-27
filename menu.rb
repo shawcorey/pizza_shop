90d2cfbdff3ed786ff03b8e4f251ea590ee4a4c4
Corey <shaw.corey@gmail.com>
2017-07-27 09:10:50 -0500

    created menu and order classes

---
+++ menu.rb
@@ -0,0 +1,8 @@
+class Menu
+  PRICE_LIST = {
+    "small" => 5.00,
+    "medium" => 7.99,
+    "large" => 11.99,
+    "xlarge" => 15.99
+  }.freeze
+end
---
+++ order.rb
@@ -0,0 +1,7 @@
+class Order
+  def initialize(first_name, last_name, phone_number, address)
+    @first_name = first_name
+    @last_name = last_name
+    @phone_number = phone_number
+    @address = address
+end
